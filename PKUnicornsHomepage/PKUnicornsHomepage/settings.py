03944fa0378a893d4db046f9267a22ffd982a0c7
yyk776 <71518858+yyk776@users.noreply.github.com>
2020-12-22 12:41:19 +0800

    Revert "add structure"

--- PKUnicornsHomepage/PKUnicornsHomepage/settings.py
+++ PKUnicornsHomepage/PKUnicornsHomepage/settings.py
@@ -31,9 +31,6 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'myHomepage',
-    'login',
-    'game',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
